d4dfa6d697852621a95afc523792276a62712593
Tochukwu <tochukwuodoeme@gmail.com>
2022-07-27 21:10:39 +0100

    add delete function and error handling manager

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,27 +1,28 @@
 import { useWorkoutsContext } from "../hooks/useWorkoutContext"
 
+//date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 const WorkoutDetails = ({workout}) => {
   const { dispatch } = useWorkoutsContext()
 
   const handleClick = async () => {
-    // const reponse = await fetch('/api/workouts/' + workout._id, {
-    //   method: 'DELETE'
-    // })
-    // const json = await reponse.json()
-    console.log(workout._id);
-    // if (reponse.ok) {
-    //   dispatch({type: 'DELETE_WORKOUT', payload: json})
-    // }
+    const reponse = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE'
+    })
+    const json = await reponse.json()
+    //console.log(workout._id);
+    if (reponse.ok) {
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
   }
   return (
     <div className="workout-details">
         <h4>{workout.title}</h4>
         <p><strong>Load (kg): </strong>{workout.load}</p>
         <p><strong>Reps: </strong>{workout.reps}</p>
-        <p>{workout.createdAt}</p>
-        <p>{workout._id}</p>
-        <span onClick={handleClick}>Delete</span>
+        <p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix: true})}</p>
+        <span className="material-symbols-outlined" onClick={handleClick}>Delete</span>
     </div>
   )
 }
